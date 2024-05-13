d6994b880385623fdd679a00f444bf9103ef57e9
Monczak <szymon.jedrzejczak.sj@gmail.com>
2024-05-13 20:28:55 +0200

    Start working on auth UI

--- src/app.py
+++ src/app.py
@@ -5,9 +5,40 @@ app = Flask(__name__,
             static_folder="../static")
 
 
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    return render_template("login.html", error="Test Error")
+
+
+@app.route("/signup", methods=["GET"])
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/signup", methods=["POST"])
+def signup_post():
+    return render_template("signup.html")
+
+
+@app.route("/verifyuser", methods=["GET"])
+def verify_user():
+    return render_template("verify-user.html")
+
+
+@app.route("/verifyuser", methods=["POST"])
+def verify_user_post():
+    return render_template("verify-user.html")
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    context = {"user_logged_in": False, "login": ""}
+    return render_template("index.html", **context)
 
 
 if __name__ == "__main__":
