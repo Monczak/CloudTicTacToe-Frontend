4ad16a87915ef80d00de0579a24a6f34392b761f
Monczak <szymon.jedrzejczak.sj@gmail.com>
2024-05-14 16:02:23 +0200

    Add refresh token logic (untested)

--- src/app.py
+++ src/app.py
@@ -11,6 +11,12 @@ app = Flask(__name__,
 app.secret_key = os.environ["FLASK_SECRET_KEY"]
 
 
+def set_tokens(response, access_token, refresh_token, expires_in):
+    response.set_cookie("AccessToken",  access_token, max_age=expires_in)
+    response.set_cookie("RefreshToken", refresh_token, max_age=expires_in)
+    return response
+
+
 @app.route("/login", methods=["GET"])
 def login():
     return render_template("login.html")
@@ -27,8 +33,7 @@ def login_post():
     
     if response_json["intent"] == "success":
         server_response = make_response(redirect("/"))
-        server_response.set_cookie("AccessToken", response_json["access_token"], max_age=int(response_json["expires_in"]))
-        server_response.set_cookie("RefreshToken", response_json["refresh_token"], max_age=int(response_json["expires_in"]))
+        set_tokens(server_response, response_json["access_token"], response_json["refresh_token"], int(response_json["expires_in"]))
         return server_response
 
     return render_template("login.html", error=response_json["description"])
@@ -47,9 +52,7 @@ def signup_post():
     response = requests.post(f"{AUTH_ADDRESS}/auth?action=signup", 
                                         json={"email": email, "username": username, "password": password}, 
                                         verify=False, timeout=30)
-    
     response_json = response.json()
-    print(response_json)
     
     if response_json["intent"] == "awaiting_verification":
         session["email"] = email
@@ -90,8 +93,7 @@ def logout():
 
     if response_json["intent"] == "success":
         server_response = make_response(redirect("/"))
-        server_response.set_cookie("AccessToken", "", expires=0)
-        server_response.set_cookie("RefreshToken", "", expires=0)
+        set_tokens(server_response, "", "", 0)
         return server_response
     return f"Couldn't log out: {response_json['description']}", 400
 
@@ -99,16 +101,37 @@ def logout():
 @app.route("/")
 def index():
     access_token = request.cookies.get("AccessToken")
+    refresh_token = request.cookies.get("RefreshToken")
+
     if access_token is None:
         return render_template("index.html", user_logged_in=False)
     
     response = requests.get(f"{AUTH_ADDRESS}/auth/get_user", 
                             headers={"Authorization": f"Bearer {access_token}"}, 
                             verify=False, timeout=30)
+    
     response_json = response.json()
+
+    if response.status_code == 401:
+        refresh_response = requests.post(f"{AUTH_ADDRESS}/auth?action=refresh", 
+                            json={"REFRESH_TOKEN": refresh_token}, 
+                            verify=False, timeout=30)
+        
+        response_json = refresh_response.json()
+        if response_json["intent"] == "success":
+            user_response = requests.get(f"{AUTH_ADDRESS}/auth/get_user", 
+                            headers={"Authorization": f"Bearer {access_token}"}, 
+                            verify=False, timeout=30)
+    
+            user_response_json = user_response.json()
+
+            server_response = make_response(render_template("index.html", user_logged_in=True, username=user_response_json["username"]))
+            set_tokens(server_response, response_json["access_token"], response_json["refresh_token"], int(response_json["expires_in"]))
+            return server_response
 
     if response_json["intent"] == "success":
         return render_template("index.html", user_logged_in=True, username=response_json["username"])
+    
     return render_template("index.html", user_logged_in=False)
 
 
